2f95d3fec58c4f92e0256814132ac22213113e9e
Vince Au <vinceau09@gmail.com>
2014-10-14 15:27:47 +1100

    Incorporated default template.

---
+++ distBakery.c
@@ -0,0 +1,24 @@
+/* distBakery.c: distributed bakery simulation program
+
+   Name: Vincent Au
+   StudentId: u5388374
+
+   ***Disclaimer***: (modify as appropriate)
+   The work that I am submitting for this program is without significant
+   contributions from others (excepting course staff).
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <unistd.h>     /* fork(), pipe(), read(), write() */
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#include "bakeryParam.h"
+#include "bakeryState.h"
+
+int main(int argc, char *argv[]) {
+  return (0);
+} //main()
